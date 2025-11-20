8e154ee0284b3e51dc6f0dc597606e6f1cb5aac4
Alex <strelchenko.aleksandr@yandex.ru>
2025-11-20 22:16:55 +0300

    feat: implement player registration

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,15 +1,71 @@
 import 'dotenv/config';
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
+import { randomUUID } from 'crypto';
 
 const PORT: number = Number(process.env.WS_PORT || 3000);
 
 const wss = new WebSocketServer({ port: PORT })
 
+interface IWebSocket extends WebSocket {
+  id: string;
+  playerName: string;
+}
+
+interface IPlayer {
+  index: string,
+  name: string,
+  password: string,
+  wins: number,
+  socket: IWebSocket
+}
+
+interface IDataBase {
+  players: IPlayer[],
+  rooms: unknown[],
+  games: unknown[]
+}
+
+const DB: IDataBase = {
+  players: [],
+  rooms: [],
+  games: [],
+}
+
 export const launchWS = () => {
-  wss.on('connection', function connection(ws) {
+  wss.on('connection', function connection(ws: IWebSocket) {
     console.log('WebSocket connected');
+    
     ws.on('message', (msg) => {
-      console.log(msg);
+      const info = JSON.parse(msg.toString());
+      const data = JSON.parse(info.data);
+      const type = info.type;
+      
+      console.log('INFO', info);
+      console.log('DATA', data);
+      console.log('TYPE', type);
+      
+      switch (type) {
+        case 'reg':
+
+        const result = checkExistedUserInDB(ws, data);
+        const JSONData = JSON.stringify({
+          name: data.name,
+          index: result.index,
+          error: result.error,
+          errorText: result.errorText
+        });
+
+        const response = {
+          type: 'reg',
+          data: JSONData,
+          id: 0
+        }
+
+        ws.send(JSON.stringify(response))
+          
+
+        break;
+      }
     })
 
     ws.on('error', console.error);
@@ -18,4 +74,41 @@ export const launchWS = () => {
   wss.on('listening', () => {
     console.log('WebSocket working...');
   })
+}
+
+
+function checkExistedUserInDB(ws: IWebSocket, data: Omit<IPlayer, 'index'>) {
+
+  const playerInDB = DB.players.find(player => player.name === data.name);
+
+  if (playerInDB) {
+    if (playerInDB.password === data.password) {
+      ws.playerName = data.name;
+
+      return {
+        index: playerInDB.index,
+        error: false
+      }
+    } else {
+      return {
+        error: true,
+        errorText: 'Wrong credentials'
+      }
+    }
+  } else {
+    ws.playerName = data.name;
+    const newPlayer = {
+      index: randomUUID(),
+      ...data,
+      wins: 0,
+      socket: ws
+    }
+
+    DB.players.push(newPlayer)
+
+    return {
+      index: newPlayer.index,
+      error: false,
+    }
+  }
 }
